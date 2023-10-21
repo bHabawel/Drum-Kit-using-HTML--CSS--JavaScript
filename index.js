46af348d2ed5eaa62d0019422cf5d22b0723d1c5
Brent Ross Habawel <rosshabawel12@gmail.com>
2023-10-21 18:03:45 +0800

    added html, css structure and few js functions.

---
+++ index.js
@@ -0,0 +1,39 @@
+let n;
+for(n = 0; n <= 6; n++){
+    document.querySelectorAll("button")[n].addEventListener("click", function (){
+
+        let button = this.innerHTML;
+        switch(button){
+            case "w":
+                var audio = new Audio("./sounds/crash.mp3");
+                audio.play();
+                break;
+            case "a": 
+                var audio = new Audio("./sounds/kick-bass.mp3");
+                audio.play();
+                break;
+            case "s":
+                var audio = new Audio("./sounds/snare.mp3");
+                audio.play();
+                break;
+            case "d":
+                var audio = new Audio("./sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "j":
+                var audio = new Audio("./sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "k":
+                var audio = new Audio("./sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case "l":
+                var audio = new Audio("./sounds/tom-4.mp3");
+                audio.play();
+                break;
+                    
+        }
+    });
+}
+
