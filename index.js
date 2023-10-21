0368a84b1abec6ffff08863fc3799e4c6f75ecda
Brent Ross Habawel <rosshabawel12@gmail.com>
2023-10-21 18:29:15 +0800

    completed project. added all of the funct needed

--- index.js
+++ index.js
@@ -1,39 +1,61 @@
-let n;
-for(n = 0; n <= 6; n++){
+// Making sounds using mouse clicks
+for(let n = 0; n <= 6; n++){
     document.querySelectorAll("button")[n].addEventListener("click", function (){
 
         let button = this.innerHTML;
-        switch(button){
-            case "w":
-                var audio = new Audio("./sounds/crash.mp3");
-                audio.play();
-                break;
-            case "a": 
-                var audio = new Audio("./sounds/kick-bass.mp3");
-                audio.play();
-                break;
-            case "s":
-                var audio = new Audio("./sounds/snare.mp3");
-                audio.play();
-                break;
-            case "d":
-                var audio = new Audio("./sounds/tom-1.mp3");
-                audio.play();
-                break;
-            case "j":
-                var audio = new Audio("./sounds/tom-2.mp3");
-                audio.play();
-                break;
-            case "k":
-                var audio = new Audio("./sounds/tom-3.mp3");
-                audio.play();
-                break;
-            case "l":
-                var audio = new Audio("./sounds/tom-4.mp3");
-                audio.play();
-                break;
-                    
-        }
+        makeSound(button);
+        drumAnimation(button);
     });
 }
 
+
+// Making sounds using keyboard presses
+document.addEventListener("keypress", function (event){
+    makeSound(event.key);
+    drumAnimation(event.key);
+});
+
+// Function that makes the sounds
+function makeSound(key){
+    switch(key){
+        case "w":
+            var audio = new Audio("./sounds/crash.mp3");
+            audio.play();
+            break;
+        case "a": 
+            var audio = new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("./sounds/snare.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("./sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("./sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("./sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("./sounds/tom-4.mp3");
+            audio.play();
+            break;
+                
+    }
+};
+
+// Making animation when a drum is played
+function drumAnimation(currentKey){
+    let currentDrum = document.querySelector("." + currentKey);
+    currentDrum.classList.add("pressed");
+
+    setTimeout(function (){
+        currentDrum.classList.remove("pressed");
+    }, 100);
+};
